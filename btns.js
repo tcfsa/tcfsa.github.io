1e3ab9c239a96768e318da36e34f81ff2f02cfef
tcfsa <fsastudentacc@gmail.com>
2025-12-02 07:26:59 +0000

    Add files via upload

---
+++ btns.js
@@ -0,0 +1,35 @@
+var temp_semesterList = null;
+var k=0;
+
+function toggleSec(courseName) {
+  const semesterList = document.getElementById(courseName);
+
+  k=0;
+
+  if(temp_semesterList!=null)
+  {
+    if(semesterList!=temp_semesterList||k==0)
+    {
+      temp_semesterList.style.display = "none";
+      temp_semesterList=null;
+    }
+  }
+
+  // Toggle the visibility of the semester list
+  if (semesterList.style.display === "none" || semesterList.style.display === "") {
+    semesterList.style.display = "block"; // Show the semesters
+  } else {
+    semesterList.style.display = "none"; // Hide the semesters
+  }
+  if(temp_semesterList!=null)
+  {
+    temp_semesterList.style.display = "block";
+  }
+}
+
+function keep(courseName) {
+  const semesterList = document.getElementById(courseName);
+
+  temp_semesterList=semesterList;
+  k=1;
+}
