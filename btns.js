9e12c605593650aa9f421a052d8778040a8e87b9
tcfsa <fsastudentacc@gmail.com>
2025-12-02 20:10:58 +0000

    Update btns.js

--- btns.js
+++ btns.js
@@ -16,10 +16,12 @@ function toggleSec(courseName) {
     }
   }
 
+  var def_color = (window.location.href=="https://tcfsa.github.io/") ? '#8A8A8A' : '#575757';
+
   // Toggle the visibility of the semester list
   if (semesterList.style.display === "none" || semesterList.style.display === "") {
     semesterList.style.display = "block"; // Show the semesters
-     document.documentElement.style.setProperty('--course-item-hover-clr', '#575757');
+     document.documentElement.style.setProperty('--course-item-hover-clr', def_color);
   } else {
     semesterList.style.display = "none"; // Hide the semesters
      document.documentElement.style.setProperty('--course-item-hover-clr', '#3890f5');
@@ -27,7 +29,7 @@ function toggleSec(courseName) {
   if(temp_semesterList!=null)
   {
       temp_semesterList.style.display = "block";
-     document.documentElement.style.setProperty('--course-item-hover-clr', '#575757');
+     document.documentElement.style.setProperty('--course-item-hover-clr', def_color);
     
   }
 }
@@ -39,5 +41,6 @@ function keep(courseName) {
   k=1;
 
 }
+
 
 
